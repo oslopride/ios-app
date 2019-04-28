f51076c3010503d7ce48b8f39666122f9892e32e
Adrian Fjelle Evensen <guleadrian@gmail.com>
2019-04-28 18:34:36 +0200

    Initial Commit

---
+++ Oslo Pride/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Oslo Pride
+//
+//  Created by Adrian Evensen on 28/04/2019.
+//  Copyright © 2019 Adrian Evensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
