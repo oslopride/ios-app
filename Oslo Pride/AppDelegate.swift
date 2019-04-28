1d5ec3cc6e7b1441b77e29f48e6e58bf3ec4d301
Adrian Fjelle Evensen <guleadrian@gmail.com>
2019-04-28 19:40:55 +0200

    init

--- Oslo Pride/AppDelegate.swift
+++ Oslo Pride/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // This setup bypasses need for Storyboards
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        let rootController = ViewController()
+        window?.rootViewController = rootController
+
         return true
     }
 
---
+++ README.mk
@@ -0,0 +1,23 @@
+## Pitch/Beskrivelse
+
+1. Tilby et slags minimums produkt i 2019, for å komme i gang og lære hva som funker / ikke funker til 2020.
+2. Utvikle location-monitoring til 2020.
+
+#### 2019
+Tilby oversikt over Oslo Pride som:
+- Kart - vise hvor de ulike arrangementete skjer
+- Liste/kalender over alle arrangementene
+- Mulighet til å skreddersy sitt eget program og få oppdateringer på det
+
+#### 2020
+Med [iBeacon](https://developer.apple.com/ibeacon/) kan vi spore hvor folk er, hvor lenge de er der og hvor de gikk etterpå.
+Kan brukes til planlegging og dødskule features som: 
+- Live oversikt over hvor folk er
+- Brukstatistikk
+- etc
+
+#### iBeacon vs GPS, Glonass etc
+- iBeacon sporer kun der vi legger ut iBeacons.
+- iBeacon har bedre oppløsning.
+- iBeacon er upåvirket av bygg og vær.
+
