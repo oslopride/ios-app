97f07b630934afc49b798ae7f7a9ada846bf755a
Adrian Fjelle Evensen <guleadrian@gmail.com>
2019-05-17 15:48:48 +0200

    Add download controller

---
+++ Oslo Pride/Controller/DownloadController.swift
@@ -0,0 +1,9 @@
+//
+//  DownloadController.swift
+//  Oslo Pride
+//
+//  Created by Adrian Evensen on 17/05/2019.
+//  Copyright © 2019 Adrian Evensen. All rights reserved.
+//
+
+import Foundation
