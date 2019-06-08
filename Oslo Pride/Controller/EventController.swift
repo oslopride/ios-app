eb9c72e712becd5f1b570451b52b2ffa890e307b
Adrian Fjelle Evensen <guleadrian@gmail.com>
2019-06-08 15:55:27 +0200

    Add share event controller, move items up and title down

--- Oslo Pride/Controller/EventController.swift
+++ Oslo Pride/Controller/EventController.swift
@@ -32,8 +32,18 @@ class EventController: UIViewController {
         imageView.clipsToBounds = true
         imageView.image = UIImage(named: "trekanter")
         imageView.contentMode = .scaleAspectFit
-        
+        imageView.layer.cornerRadius = 9
+        //imageView.layer.cornerCurve = .continuous
         return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.numberOfLines = 0
+        
+        return label
     }()
     
     let descriptionLabel: UILabel = {
@@ -89,12 +99,36 @@ class EventController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        title = event?.title
+        setupNavigationItems()
         setupLayout()
+    }
+    
+    fileprivate func setupNavigationItems() {
+        var right = [
+            UIBarButtonItem(image: UIImage(named: "share"), style: .plain, target: self, action: #selector(shareEvent)),
+            UIBarButtonItem(image: UIImage(named: "star_border"), style: .plain, target: self, action: #selector(toggleFavourite))
+        ]
+        
+        if let _ = event?.ticketSaleWebpage {
+            let ticket = UIBarButtonItem(title: "🎟 Billett", style: .plain, target: self, action: #selector(displaySalesWebpage))
+            ticket.tintColor = .prideRed
+            right.append(ticket)
+        }
+        
+        navigationItem.rightBarButtonItems = right
+    }
+    
+    @objc fileprivate func shareEvent() {
+        guard let id = event?.id else { return }
+        guard let eventURLString = URL(string: "https://www.oslopride.no/events/\(id)") else { return }
+        let shareController = UIActivityViewController(activityItems: [eventURLString], applicationActivities: [])
+        present(shareController, animated: true, completion: nil)
     }
     
     fileprivate func setupUI() {
         guard let event = event else { return }
+        
+        titleLabel.text = event.title
         
         // Display description
         descriptionLabel.text = event.eventDescription
@@ -136,33 +170,39 @@ class EventController: UIViewController {
 
         scrollView.addSubview(imageView)
         [
-            imageView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            imageView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
             imageView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 0),
-            imageView.rightAnchor.constraint(equalTo: view.rightAnchor),
-            imageView.heightAnchor.constraint(equalToConstant: view.frame.width*0.618033989)
+            imageView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+            imageView.heightAnchor.constraint(equalToConstant: (view.frame.width*0.618033989) - 20)
             ].forEach { $0.isActive = true }
         
+        scrollView.addSubview(titleLabel)
+        [
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 18),
+            titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            titleLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+            ].forEach { $0.isActive = true }
         
         scrollView.addSubview(detailsStackView)
         [
             detailsStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
             detailsStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
-            detailsStackView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 24),
+            detailsStackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 18),
             detailsStackView.bottomAnchor.constraint(lessThanOrEqualTo: scrollView.bottomAnchor, constant: -24)
             ].forEach { $0.isActive = true }
 
-        scrollView.addSubview(actionsStackView)
-        [
-            actionsStackView.centerYAnchor.constraint(equalTo: detailsStackView.centerYAnchor),
-            actionsStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
-            actionsStackView.heightAnchor.constraint(equalToConstant: 55),
-            actionsStackView.leftAnchor.constraint(equalTo: view.centerXAnchor)
-            ].forEach { $0.isActive = true }
+//        scrollView.addSubview(actionsStackView)
+//        [
+//            actionsStackView.centerYAnchor.constraint(equalTo: detailsStackView.centerYAnchor),
+//            actionsStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+//            actionsStackView.heightAnchor.constraint(equalToConstant: 55),
+//            actionsStackView.leftAnchor.constraint(equalTo: view.centerXAnchor)
+//            ].forEach { $0.isActive = true }
         
         scrollView.addSubview(descriptionLabel)
         [
-            descriptionLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24),
-            descriptionLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24),
+            descriptionLabel.leftAnchor.constraint(equalTo: detailsStackView.leftAnchor),
+            descriptionLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
             descriptionLabel.topAnchor.constraint(equalTo: detailsStackView.bottomAnchor, constant: 10),
             descriptionLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -24)
             ].forEach { $0.isActive = true }
