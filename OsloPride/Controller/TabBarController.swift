4aaf5e240b5b3e18accb8d8f20b49b4fa158dbd7
Elvis Nuñez <elvisnunez@me.com>
2019-10-29 17:39:00 +0100

    Make program the first thing

--- OsloPride/Controller/TabBarController.swift
+++ OsloPride/Controller/TabBarController.swift
@@ -4,13 +4,16 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let viewController = EventsController()
-        viewController.tabBarItem = UITabBarItem(title: "Program", image: UIImage(named: "event_twotone"), tag: 0)
+        let eventsController = EventsController()
+        eventsController.tabBarItem = UITabBarItem(title: "Program", image: UIImage(named: "event_twotone"), tag: 0)
+
+        let eventsNavigationController = UINavigationController(rootViewController: eventsController)
+        eventsNavigationController.view.backgroundColor = .white
 
         let mapController = MapController()
-        let mapNavController = UINavigationController(rootViewController: mapController)
-        mapNavController.tabBarItem = UITabBarItem(title: "Kart", image: UIImage(named: "map_twotone"), tag: 1)
-        mapNavController.isNavigationBarHidden = true
+        let mapNavigationController = UINavigationController(rootViewController: mapController)
+        mapNavigationController.tabBarItem = UITabBarItem(title: "Kart", image: UIImage(named: "map_twotone"), tag: 1)
+        mapNavigationController.isNavigationBarHidden = true
         
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.itemSize = CGSize(width: view.frame.width - 14 * 2, height: 440)
@@ -19,22 +22,20 @@ class TabBarController: UITabBarController {
         let favouriteController = FavouriteController(collectionViewLayout: flowLayout)
         favouriteController.collectionView.contentInset = .init(top: 24, left: 0, bottom: 24, right: 0)
         
-        let favouriteNavController = UINavigationController(rootViewController: favouriteController)
-        favouriteNavController.tabBarItem = UITabBarItem(title: "Favoritter", image: UIImage(named: "star_twotone"), tag: 2)
-        favouriteNavController.view.backgroundColor = .white
-        favouriteNavController.isNavigationBarHidden = true
-        tabBar.tintColor = .prideRed
-        
-        let navViewController = UINavigationController(rootViewController: viewController)
-        navViewController.view.backgroundColor = .white
-        
+        let favouriteNavigationController = UINavigationController(rootViewController: favouriteController)
+        favouriteNavigationController.tabBarItem = UITabBarItem(title: "Favoritter", image: UIImage(named: "star_twotone"), tag: 2)
+        favouriteNavigationController.view.backgroundColor = .white
+        favouriteNavigationController.isNavigationBarHidden = true
+
         let infoController = InfoController()
         infoController.tabBarItem = UITabBarItem(title: "Info", image: UIImage(named: "info"), tag: 3)
-        
+
+        tabBar.tintColor = .prideRed
+
         viewControllers = [
-            favouriteNavController,
-            navViewController,
-            mapNavController,
+            eventsNavigationController,
+            favouriteNavigationController,
+            mapNavigationController,
             infoController
         ]
     }
