6e9678cd12af2ed06a597100e855143d7742f7a3
Elvis Nuñez <elvisnunez@me.com>
2019-10-29 17:44:20 +0100

    Remove commented code

--- OsloPride/View/EventsFilterHeaderView.swift
+++ OsloPride/View/EventsFilterHeaderView.swift
@@ -1,11 +1,3 @@
-//
-//  EventsFilterHeaderView.swift
-//  Oslo Pride
-//
-//  Created by Adrian Evensen on 28/05/2019.
-//  Copyright © 2019 Adrian Evensen. All rights reserved.
-//
-
 import UIKit
 
 class FilterStackView: UIStackView {
@@ -35,9 +27,9 @@ class EventsFilterHeaderView: UIView {
     let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        // scrollView.backgroundColor = .prideBlue
         scrollView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-        
+        scrollView.showsHorizontalScrollIndicator = false
+
         return scrollView
     }()
     
@@ -56,27 +48,17 @@ class EventsFilterHeaderView: UIView {
             scrollView.rightAnchor.constraint(equalTo: rightAnchor),
             scrollView.topAnchor.constraint(equalTo: topAnchor),
             scrollView.heightAnchor.constraint(equalToConstant: 60)
-            // scrollView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ].forEach { $0.isActive = true }
         
         let categoryStack = FilterStackView()
-        
-//        categoryStack.isLayoutMarginsRelativeArrangement = true
-//        categoryStack.layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-//        categoryStack.translatesAutoresizingMaskIntoConstraints = false
         categoryStack.axis = .horizontal
         categoryStack.spacing = 10
-        
-        // stackView.distribution = .fillEqually
-        
         scrollView.addSubview(categoryStack)
         [
             categoryStack.leftAnchor.constraint(equalTo: scrollView.leftAnchor),
             categoryStack.rightAnchor.constraint(equalTo: scrollView.rightAnchor),
             categoryStack.topAnchor.constraint(equalTo: topAnchor),
             categoryStack.heightAnchor.constraint(equalToConstant: 60)
-            // categoryStack.bottomAnchor.constraint(equalTo: bottomAnchor),
-            // stackView.heightAnchor.constraint(equalToConstant: 50)
         ].forEach { $0.isActive = true }
         
         [
@@ -167,11 +149,11 @@ class FilterButton: UIButton {
         didSet {
             UIView.animate(withDuration: 0.1, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8, options: .curveEaseInOut, animations: {
                 if self.isActivated {
-                    self.backgroundColor = self.filter.color // self.bc
+                    self.backgroundColor = self.filter.color
                     self.tintColor = self.tc
                 } else {
                     self.backgroundColor = self.tc
-                    self.tintColor = .graySuit // self.bc
+                    self.tintColor = .graySuit
                 }
             }, completion: nil)
         }
