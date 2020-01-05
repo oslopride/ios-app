e0b2fe69c71ad3290c529a0af604d0c8982bc9fa
Adrian Evensen <guleadrian@gmail.com>
2020-01-05 19:00:11 +0100

    init ProgramView master-detail flow

--- OsloPride/Controller/ProgramView.swift
+++ OsloPride/Controller/ProgramView.swift
@@ -8,14 +8,52 @@
 
 import SwiftUI
 
+struct ProgramEvent: Identifiable {
+    var id: Int
+    var text: String
+}
+
 struct ProgramView: View {
+
+    let data = [
+        ProgramEvent(id: 0, text: "hei"),
+        ProgramEvent(id: 1, text: "på"),
+        ProgramEvent(id: 2, text: "deg"),
+    ]
+
+    var body: some View {
+        NavigationView {
+            List(data) { d in
+                NavigationLink(destination: ProgramDetail(text: d.text)) {
+                    ProgramRow(text: d.text)
+                }
+            }
+            .navigationBarTitle(Text("Program"))
+        }
+    }
+    
+}
+
+struct ProgramDetail: View {
+    var text: String
+    
     var body: some View {
         VStack {
-            Text("init")
+            Text(text)
         }
     }
 }
 
+struct ProgramRow: View {
+    
+    let text: String
+    
+    var body: some View {
+        HStack {
+            Text(text).font(.title)
+        }
+    }
+}
 struct ProgramView_Preview: PreviewProvider {
     static var previews: some View {
         ProgramView()
